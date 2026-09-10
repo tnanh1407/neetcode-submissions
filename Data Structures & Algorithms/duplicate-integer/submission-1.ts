af10fb45f8d51689a9c13387117f58bc153351f3
Anh Tran Ngoc <124139561+tnanh1407@users.noreply.github.com>
2026-09-10 23:18:55 +0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.ts
@@ -0,0 +1,13 @@
+class Solution {
+    hasDuplicate(nums: number[]): boolean {
+        const len = nums.length ;
+        for(let i = 0 ; i < len - 1 ; i ++) {
+            for(let j = i + 1 ; j < len ; j++ ){
+                 if( nums[i] == nums[j]){
+                    return true ;
+                 }
+            }
+        }
+        return false ;
+    }
+}
