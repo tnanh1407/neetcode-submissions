e1ab0e932b607de93742c2af54d392f9808166a3
Anh Tran Ngoc <124139561+tnanh1407@users.noreply.github.com>
2026-09-10 23:18:20 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const len = nums.length ;
+        for(let i = 0 ; i < len - 1 ; i ++) {
+            for(let j = i + 1 ; j < len ; j++ ){
+                 if( nums[i] == nums[j]){
+                    return true ;
+                 }
+            }
+        }
+        return false ;
+    }
+}
